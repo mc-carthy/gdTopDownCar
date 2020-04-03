b95c9d889d6cab54ac483119476e8a2a9abcea02
Mick McCarthy <mick@mccarthy.tech>
2020-04-03 22:10:05 +0100

    Make use of drag, friction and engine power

--- Scripts/Car.gd
+++ Scripts/Car.gd
@@ -2,16 +2,23 @@ extends KinematicBody2D
 
 var wheelbase : float = 70.0
 var max_steering_angle : float = 15.0
+var engine_power : float = 500
+var rolling_resistance : float = -0.9
+var drag_coefficient : float = -0.001
+
+var acceleration : Vector2 = Vector2.ZERO
 var velocity : Vector2 = Vector2.ZERO
 var steering_angle : float = 0.0
-var max_speed : float = 500
 
 func _ready():
 	$Camera2D.make_current()
 
 func _physics_process(delta):
+	acceleration = Vector2.ZERO
 	get_input()
+	apply_friction(delta)
 	calculate_steering(delta)
+	velocity += acceleration * delta
 	velocity = move_and_slide(velocity)
 
 func get_input():
@@ -22,9 +29,15 @@ func get_input():
 		steering += 1
 	steering_angle = steering * deg2rad(max_steering_angle)
 	
-	velocity = Vector2.ZERO
 	if Input.is_action_pressed('forward'):
-		velocity = transform.x * max_speed
+		acceleration = transform.x * engine_power
+
+func apply_friction(delta):
+	if velocity.length() < 5:
+		velocity = Vector2.ZERO
+	var fr : Vector2 = velocity * rolling_resistance
+	var fd : Vector2 = velocity * velocity.length() * drag_coefficient
+	velocity += (fr + fd) * delta
 
 func calculate_steering(delta):
 	var rear_wheel_location : Vector2 = position - transform.x * wheelbase / 2.0
